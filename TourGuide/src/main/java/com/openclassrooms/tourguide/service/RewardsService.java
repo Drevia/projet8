b370264f747dde88c99ff6a6feb44b4c2577b97f
Cyriac <drevia59@gmail.com>
2024-01-12 17:30:51 +0100

    fix caculateRewards and User model + add lib in pom

--- TourGuide/src/main/java/com/openclassrooms/tourguide/service/RewardsService.java
+++ TourGuide/src/main/java/com/openclassrooms/tourguide/service/RewardsService.java
@@ -1,6 +1,9 @@
 package com.openclassrooms.tourguide.service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
@@ -35,22 +38,34 @@ public class RewardsService {
 	public void setDefaultProximityBuffer() {
 		proximityBuffer = defaultProximityBuffer;
 	}
-	
+
+
 	public void calculateRewards(User user) {
 		List<VisitedLocation> userLocations = user.getVisitedLocations();
 		List<Attraction> attractions = gpsUtil.getAttractions();
-		
-		for(VisitedLocation visitedLocation : userLocations) {
-			for(Attraction attraction : attractions) {
-				if(user.getUserRewards().stream().filter(r -> r.attraction.attractionName.equals(attraction.attractionName)).count() == 0) {
-					if(nearAttraction(visitedLocation, attraction)) {
-						user.addUserReward(new UserReward(visitedLocation, attraction, getRewardPoints(attraction, user)));
-					}
+
+		List<VisitedLocation> userLocationsCopy = new ArrayList<>(userLocations);
+
+		//TODO: refacto les boucle for
+		for (VisitedLocation visitedLocation : userLocationsCopy) {
+			for (Attraction attraction : attractions) {
+				if (shouldAddReward(user, attraction) && nearAttraction(visitedLocation, attraction)) {
+					addReward(user, visitedLocation, attraction);
 				}
 			}
 		}
 	}
-	
+
+	public boolean shouldAddReward(User user, Attraction attraction) {
+		return user.getUserRewards().stream()
+				.noneMatch(r -> r.attraction.attractionName.equals(attraction.attractionName));
+	}
+
+	public void addReward(User user, VisitedLocation visitedLocation, Attraction attraction) {
+		user.addUserReward(new UserReward(visitedLocation, attraction, getRewardPoints(attraction, user)));
+	}
+
+
 	public boolean isWithinAttractionProximity(Attraction attraction, Location location) {
 		return getDistance(attraction, location) > attractionProximityRange ? false : true;
 	}
@@ -73,8 +88,7 @@ public class RewardsService {
                                + Math.cos(lat1) * Math.cos(lat2) * Math.cos(lon1 - lon2));
 
         double nauticalMiles = 60 * Math.toDegrees(angle);
-        double statuteMiles = STATUTE_MILES_PER_NAUTICAL_MILE * nauticalMiles;
-        return statuteMiles;
+		return STATUTE_MILES_PER_NAUTICAL_MILE * nauticalMiles;
 	}
 
 }
